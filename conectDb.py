3d9f7fcfe1f5629a65c7b92d0c1272c0cbfdd0fd
Henrique Froeder <henriquefroeder52@gmail.com>
2023-10-27 13:23:03 -0300

    conect db

---
+++ conectDb.py
@@ -0,0 +1,5 @@
+import pyodbc
+
+# ODBC
+connection_string = 'DSN=dev_independencia;UID=henriquefroeder;PWD=Froeder@'
+
--- limpaDados.py
+++ limpaDados.py
@@ -2,10 +2,50 @@ import csv
 import json
 import openpyxl
 import re
+import pyodbc
+
+connection_string = 'DSN=dev_independencia;UID=henriquefroeder;PWD=Froeder@'
 
 #o nome precisa ser o mesmo, pq tá fixo
 xlsx_filename = "POP2022_Municipios_20230622.xlsx"
 csv_filename = "censo.csv"
+tabela_banco = "censo"
+
+def get_odbc_connection():
+    return pyodbc.connect(connection_string)
+
+try:
+    connection = get_odbc_connection()    
+    cursor = connection.cursor()
+    
+    verificaSeExiste = f"SELECT 1 FROM censo WHERE 1=0"
+    
+    try:
+        cursor.execute(verificaSeExiste)
+        print(f"A tabela 'censo' já existe. Não é necessário criá-la.")
+    except pyodbc.ProgrammingError:
+    
+        # cria a tabela
+        create_table_query = """
+        CREATE TABLE censo (
+          ID INTEGER NOT NULL DEFAULT AUTOINCREMENT PRIMARY KEY,
+          UF VARCHAR(2),
+          COD_UF VARCHAR(10),
+          COD_MUNIC VARCHAR(10),
+          NOMEDOMUNICIPIO VARCHAR(100),
+          POPULACAO VARCHAR(20)
+        )
+        """
+
+        cursor.execute(create_table_query)    
+        connection.commit()
+        print("Tabela 'censo' criada com sucesso.")    
+        
+    cursor.close()
+    connection.close()
+    
+except pyodbc.Error as e:
+    print("Erro")
 
 workbook = openpyxl.load_workbook(xlsx_filename, read_only=True)
 worksheet = workbook.active
@@ -25,6 +65,8 @@ with open(csv_filename, 'r', newline='') as csv_file:
         data.append(row)
 
 # transforma os dados do csv para um formato mais aceitavel do json
+connection = get_odbc_connection()    
+conexao = connection.cursor()
 formatted_data = {}
 for row in data:
     uf = row["UF"]
@@ -35,6 +77,11 @@ for row in data:
     populacao = re.sub(r'\([^)]*\)', '', row["POPULAÇÃO"])
     populacao = re.sub(r'[^\d,]', '', populacao)
     populacao = int(populacao.replace(',', ''))
+    
+    #quando vai criar o arquivo json e formatar ele já vai ser inserido no banco    
+    insert_query = f"INSERT INTO {tabela_banco} (UF, COD_UF, COD_MUNIC, NOMEDOMUNICIPIO, POPULACAO) VALUES (?, ?, ?, ?, ?)"
+    conexao.execute(insert_query, (uf, cod_uf, cod_municipio, nome_municipio, populacao))
+    connection.commit()
 
     #cria a estrutura principal, onde fica o estado e depois adiciona os municipios
     if uf not in formatted_data:
@@ -49,9 +96,11 @@ for row in data:
         "nome_municipio": nome_municipio,
         "populacao": populacao
     })
-
+conexao.close()
+connection.close()
 # cria o json
 json_filename = "censo.json"
+
 
 with open(json_filename, 'w') as json_file:
     json.dump(list(formatted_data.values()), json_file, indent=2)
